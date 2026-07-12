ad9ee8f2fe50c93c88c59319c5b7b090764bd5c9
woomchi <89631875+woomchi@users.noreply.github.com>
2026-07-12 15:19:55 +0900

    드래그 앤 드롭 설정 파일

---
+++ src/hooks/ui/useMobileDndSensors.ts
@@ -0,0 +1,21 @@
+import { KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
+
+export function useMobileDndSensors() {
+  return useSensors(
+    useSensor(MouseSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    }),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 250,
+        tolerance: 5,
+      },
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+}
